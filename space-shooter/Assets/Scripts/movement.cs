405bd3a183b9400b2987d253f7acea9d78dc8be2
Kyle <kyle.evitts@gmail.com>
2018-08-01 23:22:41 -0700

    copied space shooter tutorial stuffs. made new scene. Got rotation working

---
+++ space-shooter/Assets/Scripts/movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    public float rotationSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float rotation = Input.GetAxis("Rotation");
+        //Vector3 currentRotation = transform.eulerAngles;
+        //Vector3 delta = new Vector3(0, 0, rotaton);
+        transform.Rotate(0, rotationSpeed * rotation, 0);
+	}
+}
