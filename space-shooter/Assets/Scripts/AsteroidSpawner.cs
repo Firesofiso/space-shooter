4728c9757ea3353ca40818d9375d299affa56934
firesofiso <harry.bayley@gmail.com>
2018-08-05 12:16:33 -0700

    Created a new object called Asteroid spawner.  Asteroid objects now spawn off the screen every X Seconds.

---
+++ space-shooter/Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour {
+
+    public List<GameObject> asteroids = new List<GameObject>();
+    public float spawnRate;
+    private int x = 10;
+    private int y = 6;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("CreateAsteroid");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+	}
+
+    public IEnumerator CreateAsteroid()
+    {
+        while (true)
+        {
+            float randX = Random.Range(-x - 2, x + 3);
+            float randY = Random.Range(-y - 2, y + 3);
+            int coinFlip = Random.Range(0, 2);
+
+            if (randX >= -x && randX <= x)
+            {
+                if (coinFlip == 0)
+                {
+                    randY = Random.Range(y, y + 3);
+                }
+                else
+                {
+                    randY = Random.Range(-y, -y - 3);
+                }
+            }
+            else if (randY >= -y && randY <= y)
+            {
+                if (coinFlip == 0)
+                {
+                    randX = Random.Range(x, x + 3);
+                }
+                else
+                {
+                    randX = Random.Range(-x, -x - 3);
+                }
+            }
+
+            Vector3 randPos = new Vector3(randX, randY, 0);
+            GameObject tempAsteroid = Instantiate(asteroids[0], randPos, Quaternion.identity, transform);
+
+            yield return new WaitForSeconds(spawnRate);
+        }
+    }
+}
