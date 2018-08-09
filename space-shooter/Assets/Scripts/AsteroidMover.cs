f773fc74ffe08f518756b4019aa9740e417bb729
firesofiso <harry.bayley@gmail.com>
2018-08-08 23:05:22 -0700

    Asteroids now move towards a random point at a speed between the minSpeed and maxSpeed

--- space-shooter/Assets/Scripts/AsteroidMover.cs
+++ space-shooter/Assets/Scripts/AsteroidMover.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class AsteroidMover : MonoBehaviour {
 
+    private Vector3 target;
+    private Vector3 direction;
+    private float speed;
+    public float minSpeed = 1;
+    public float maxSpeed = 2;
+
 	// Use this for initialization
 	void Start () {
-		
+        target = new Vector3(Random.Range(-10, 11), Random.Range(-5, 6), 0);
+        direction = target - transform.position;
+        speed = Random.Range(minSpeed, maxSpeed);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float step = speed * Time.deltaTime;
+
+
+
+        //transform.Translate(heading * speed * Time.deltaTime);
+        transform.Translate(direction.normalized * speed, Space.World);
 	}
 }
