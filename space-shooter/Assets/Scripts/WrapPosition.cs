5fb0408f3f826ac1e0e271562aa3c1fcc77414db
Kyle <kyle.evitts@gmail.com>
2018-08-08 23:11:32 -0700

    initial commit of WrapPosition script.

---
+++ space-shooter/Assets/Scripts/WrapPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrapPosition : MonoBehaviour {
+
+    Camera Cam;
+
+	// Use this for initialization
+	void Start () {
+        Cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // look at Cam.WorldToScreenPoint(), Cam.WorldToViewportPoint, and Cam.worldToCameraMatrix and related...
+        
+	}
+}
