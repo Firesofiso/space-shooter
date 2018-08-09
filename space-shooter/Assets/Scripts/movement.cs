f818b5558f5d930f3237934d46136d5bae235a6b
Kyle <kyle.evitts@gmail.com>
2018-08-08 22:43:07 -0700

    Initial forward movement working.

--- space-shooter/Assets/Scripts/movement.cs
+++ space-shooter/Assets/Scripts/movement.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class movement : MonoBehaviour {
 
     public float rotationSpeed = 1;
+    public float accelerationCoef = 1;
+    private Vector3 currentVelocity = new Vector2();
 
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        // Rotation stuff
         float rotation = Input.GetAxis("Rotation");
-        //Vector3 currentRotation = transform.eulerAngles;
-        //Vector3 delta = new Vector3(0, 0, rotaton);
         transform.Rotate(0, rotationSpeed * rotation, 0);
+
+        // Forward movement stuff
+        float fowardAxis = Input.GetAxis("Forward Momentum");
+        Vector3 accelerationVect = transform.forward * fowardAxis * accelerationCoef;
+        float deltaT = Time.deltaTime; // This might not be the right time property.
+        
+        currentVelocity += accelerationVect * Time.deltaTime;
+        Vector3 displacementVect = currentVelocity * Time.deltaTime;
+        transform.Translate(displacementVect, Space.World);
 	}
 }
