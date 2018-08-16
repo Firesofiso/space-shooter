86933716fe5a199094921a40f4026ec4a4e624e3
firesofiso <harry.bayley@gmail.com>
2018-08-15 23:06:24 -0700

    Colliding with an asteroid resets the scene

---
+++ space-shooter/Assets/Scripts/DestroyOnContact.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyOnContact : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            return;
+        }
+
+        if (other.tag == "Player")
+        {
+            // Here is where we reset the scene.
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
