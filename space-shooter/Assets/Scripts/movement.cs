373b8ac0b888d14566f54b925ba2af60ff737a2c
firesofiso <harry.bayley@gmail.com>
2018-08-15 22:47:40 -0700

    Jet and Flame particle now increase based on velocity in a certain direction

--- space-shooter/Assets/Scripts/movement.cs
+++ space-shooter/Assets/Scripts/movement.cs
@@ -8,10 +8,13 @@ public class movement : MonoBehaviour {
     public float accelerationCoef = 1;
     private Vector3 currentVelocity = new Vector2();
 
+    private ParticleSystem flame, jet;
+
 	// Use this for initialization
 	void Start () {
-        
-	}
+        flame = transform.GetChild(0).GetComponent<ParticleSystem>();
+        jet = transform.GetChild(1).GetComponent<ParticleSystem>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,12 +23,18 @@ public class movement : MonoBehaviour {
         transform.Rotate(0, rotationSpeed * rotation, 0);
 
         // Forward movement stuff
-        float fowardAxis = Input.GetAxis("Forward Momentum");
-        Vector3 accelerationVect = transform.forward * fowardAxis * accelerationCoef;
+        float forwardAxis = Input.GetAxis("Forward Momentum");
+        Vector3 accelerationVect = transform.forward * forwardAxis * accelerationCoef;
         float deltaT = Time.deltaTime; // This might not be the right time property.
         
         currentVelocity += accelerationVect * Time.deltaTime;
         Vector3 displacementVect = currentVelocity * Time.deltaTime;
         transform.Translate(displacementVect, Space.World);
-	}
+
+        // Increase the size of the flames based of forwardAxis
+        ParticleSystem.MainModule flameMain = flame.main;
+        flameMain.startSize = (forwardAxis/2);
+        ParticleSystem.MainModule jetMain = jet.main;
+        jetMain.startSize = (forwardAxis/2) + 0.15f;
+    }
 }
