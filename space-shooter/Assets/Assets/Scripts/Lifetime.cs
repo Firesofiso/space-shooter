be254459d0ed329362bbf4cb72a553f53944d820
firesofiso <harry.bayley@gmail.com>
2018-11-14 22:05:41 -0800

    Created a lifetime script that destroys an object after a set amount of seconds.  Added it to the bullet prefab.

---
+++ space-shooter/Assets/Assets/Scripts/Lifetime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lifetime : MonoBehaviour {
+
+    public int lifetimeInSeconds;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(destroyAfterLifetime());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public IEnumerator destroyAfterLifetime()
+    {
+        yield return new WaitForSeconds(lifetimeInSeconds);
+        Destroy(gameObject);
+    }
+}
