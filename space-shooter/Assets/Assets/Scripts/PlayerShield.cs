5a6fa07b59eee8a11382c82063261ed181aa76f4
firesofiso <harry.bayley@gmail.com>
2019-01-05 13:48:46 -0800

    I have no idea what this commit is, its been way too long!

---
+++ space-shooter/Assets/Assets/Scripts/PlayerShield.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShield : MonoBehaviour {
+
+    public float duration = 1;
+    public float speed = 100;
+    public Quaternion startRotation;
+    public Quaternion targetRotation;
+
+	// Use this for initialization
+	void Start () {
+        //targetRotation = Random.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //var step = 100 * speed * Time.deltaTime;
+        //transform.Rotate(new Vector3(Random.Range(0, 180), Random.Range(0, 180), Random.Range(0, 180)), Space.Self);
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, step);
+        //if (transform.rotation == targetRotation) targetRotation = Random.rotation;
+	}
+}
