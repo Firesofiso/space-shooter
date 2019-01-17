46e75d85c588397240e91c203bd1de9b802ad2de
firesofiso <harry.bayley@gmail.com>
2019-01-16 20:25:12 -0800

    Moved around some scripts, Added a new Main Menu scene and Created a slow scroll script for the background

---
+++ space-shooter/Assets/Assets/Scripts/BackgroundMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMover : MonoBehaviour {
+
+	private Material material;
+
+	// Use this for initialization
+	void Start () {
+		material = GetComponent<MeshRenderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float offset = Time.time * 0.05f;
+		material.SetTextureOffset("_MainTex", new Vector2(offset, -offset));
+	}
+}
