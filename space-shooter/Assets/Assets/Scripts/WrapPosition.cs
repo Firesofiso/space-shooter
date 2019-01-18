03855d1d19b48a501df8df19f9eb5008aa62c208
Kyle <kyle.evitts@gmail.com>
2019-01-17 20:28:46 -0800

    Improved wrap position script to reduce jumpyness.

--- space-shooter/Assets/Assets/Scripts/WrapPosition.cs
+++ space-shooter/Assets/Assets/Scripts/WrapPosition.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class WrapPosition : MonoBehaviour {
 
     private Camera Cam;
-    public static int buffer;
+    public float BufferFactor = 0.05f;
+    private int buffer;
 
 	// Use this for initialization
 	void Start () {
         //Cam = GetComponent<Camera>(); // this didn't work for some reason...
         Cam = Camera.main;
-        buffer = Cam.pixelHeight / 20;
+        buffer = (int)(Cam.pixelHeight * BufferFactor);
 	}
 	
 	// Update is called once per frame
@@ -19,21 +20,24 @@ public class WrapPosition : MonoBehaviour {
         // look at Cam.WorldToScreenPoint(), Cam.WorldToViewportPoint, and Cam.worldToCameraMatrix and related...
         Vector3 screenSpaceLoc = Cam.WorldToScreenPoint(transform.position);
 
+        float shiftedX = screenSpaceLoc.x + buffer;
+        float shiftedY = screenSpaceLoc.y + buffer;
+
         // deal with negative values
-        if (screenSpaceLoc.x < 0)
+        if (shiftedX < 0)
         {
-            screenSpaceLoc.x = Cam.pixelWidth + buffer;
+            shiftedX = Cam.pixelWidth + buffer;
         }
-        if (screenSpaceLoc.y < 0)
+        if (shiftedY < 0)
         {
-            screenSpaceLoc.y = Cam.pixelHeight + buffer;
+            shiftedY = Cam.pixelHeight + buffer;
         }
 
-        int modX = Cam.pixelWidth + buffer + 1;
-        int modY = Cam.pixelHeight + buffer + 1;
+        int modX = Cam.pixelWidth + (2 * buffer) + 1;
+        int modY = Cam.pixelHeight + (2 * buffer) + 1;
 
-        screenSpaceLoc.x = screenSpaceLoc.x % modX;
-        screenSpaceLoc.y = screenSpaceLoc.y % modY;
+        screenSpaceLoc.x = (shiftedX % modX) - buffer;
+        screenSpaceLoc.y = (shiftedY % modY) - buffer;
         transform.position = Cam.ScreenToWorldPoint(screenSpaceLoc);
 	}
 }
