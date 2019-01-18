37af2cff50f3132bcc78445e55be00a90d1b6742
Kyle <kyle.evitts@gmail.com>
2019-01-17 19:48:49 -0800

    Tuned some coefficients for the game.

--- space-shooter/Assets/Assets/Scripts/Lifetime.cs
+++ space-shooter/Assets/Assets/Scripts/Lifetime.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Lifetime : MonoBehaviour {
 
-    public int lifetimeInSeconds;
+    public float lifetimeInSeconds;
 
 	// Use this for initialization
 	void Start () {
