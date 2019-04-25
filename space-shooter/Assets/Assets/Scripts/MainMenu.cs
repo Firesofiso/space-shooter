818fc07c4ad5f9dadfc12094ce41cf90c27cd447
firesofiso <harry.bayley@gmail.com>
2019-04-24 19:15:34 -0700

    started on the main menu

--- space-shooter/Assets/Assets/Scripts/MainMenu.cs
+++ space-shooter/Assets/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
+
+	public Button b_Start;
 
 	// Use this for initialization
 	void Start () {
